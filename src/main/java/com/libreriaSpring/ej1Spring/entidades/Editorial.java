4869611b27da1cc12b275ebd95edd2928e931943
leonahuelfrias <leonahuelfrias@gmail.com>
2021-10-28 12:49:34 -0300

    creacion de entidades

---
+++ src/main/java/com/libreriaSpring/ej1Spring/entidades/Editorial.java
@@ -0,0 +1,56 @@
+package com.libreriaSpring.ej1Spring.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Editorial {
+    
+    @Id
+    private String id;
+    private String nombre;
+    private Boolean alta;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the alta
+     */
+    public Boolean getAlta() {
+        return alta;
+    }
+
+    /**
+     * @param alta the alta to set
+     */
+    public void setAlta(Boolean alta) {
+        this.alta = alta;
+        
+    }
+}
---
+++ src/main/java/com/libreriaSpring/ej1Spring/entidades/Libro.java
@@ -0,0 +1,26 @@
+package com.libreriaSpring.ej1Spring.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Libro {
+    
+    @Id
+    private String id;
+    private Long isbn;
+    private String titulo;
+    private Integer anio;
+    private Integer ejemplares;
+    private Integer ejemplaresPrestados;
+    private Integer ejemplaresRestantes;
+    private Boolean alta;
+    
+    @ManyToOne
+    private Autor autor;
+    
+    @ManyToOne
+    private Editorial editorial;
+    
+}
